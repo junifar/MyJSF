0d5f3cc2063d8ac6e25658deb87834e3d41baec9
junifar <junifar@gmail.com>
2014-04-15 06:49:42 +0700

    tambahan redirect

---
+++ Bank1/src/java/Utils/BaseUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+/**
+ *
+ * @author prasetia
+ */
+public class BaseUtil {
+
+    /**
+     *
+     * @param ex
+     */
+    public static void errorTrackingApp(Exception ex)
+    {
+        System.err.println("Aplikasi Error" + ex);
+    }
+}
